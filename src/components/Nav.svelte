732e75ce0e3b4d666cbb0e2787720e637351b57f
AnotherSii <davidwillbedunn@gmail.com>
2021-01-06 18:58:54 -0800

    html

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -10,15 +10,15 @@
       margin: 0;
    }
     img {
-        margin-left: 10px;
-        margin-top: 10px;
+        margin: 10px;
+        width: 5%;
+        height: 5%;
     }
 </style>
 
 <nav>
-   <img alt='Token' src='purple_circle.png' style="width:5%; height:5%;">
+   <img alt='Token' src='purple_circle.png'>
     <div style="color:#4f00e0">
-        <p>BLOCKCHAIN</p> 
-        <p>ACCOUNTING</p>
+        <p><b>BLOCKCHAIN <br> ACCOUNTING</b></p>
        </div>
 </nav>
