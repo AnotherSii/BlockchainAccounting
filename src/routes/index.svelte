41e45e74ee033c8188704d1c513d3a5e95c99810
AnotherSii <davidwillbedunn@gmail.com>
2021-01-07 01:44:15 -0800

    lamden wallet

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,7 +11,7 @@
         networkType: 'testnet', // other option is 'mainnet'
     });
     
-    document.dispatchEvent(new CustomEvent('lamdenWalletConnect', { detail } ));
+    window.dispatchEvent(new CustomEvent('lamdenWalletConnect', { detail } ));
 
 
     
